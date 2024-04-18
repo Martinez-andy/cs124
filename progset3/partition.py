75cb063019882bcfa90888052e0fc584c935fdfb
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-04-18 00:56:57 -0400

    Implemented pre-part and KK alg

--- progset3/partition.py
+++ progset3/partition.py
@@ -1,13 +1,77 @@
+import random
+import heapq
 import sys
 
 
+# Takes in sequence A and returns A' (used for pre-partitioned algs)
+def prepart(A):
+    # Initialize A to be list of zeros
+    A_prime = [0 for _ in A]
+    
+    # Store length of A for pretty code
+    n = len(A)
+    
+    # For each entry...
+    for i in range(n):
+        # Select random int from 1 to n, the same as creating some P but more memory efficient
+        p_i = random.randint(1, n)
+        
+        # Update A_prime accoring to notes
+        A_prime[p_i - 1] += A[i]
+    return A
+
+
+# KK algorithm
+def karmarkarKarp(A):
+    # Negate entries to make the min-heap a max-heap
+    A = [-a for a in A]
+    
+    # Heapify list and initialize return val
+    A_heap = heapq.heapify(A)
+
+    # Exit when only one element is left
+    while len(A_heap) == 1:
+        
+        # Take largest 2 elements (can do blindly because of while condition)
+        top_1 = heapq.heappop(A_heap)
+        top_2 = heapq.heappop(A_heap)
+        
+        # Calculate residual
+        res = abs(top_1 - top_2)
+        
+        # Only add residual back if non-zero, else return 0
+        if not res:
+            return res
+        heapq.heappush(A_heap, -res)
+
+
+    return A_heap[0]
+    
+
+
+# Implementation of repeated random alg
+def repeatedRandom(A):
+    return None
+
+
+# Implementatoin of hill climb alg
+def hillClimbing(A):
+    return None
+
+
+# Implementation of simulated Annealing alg
+def simulatedAnnealing(A):
+    return None
+
+
+# Code that handles inputs and calls algorithms
 def main():
     if len(sys.argv) != 4:
         raise ValueError(f"Improper input lenght. file ")
     
-    number = []
+    A = []
     
-    # Read file input
+    # Read file input/extract number list
     with open(sys.argv[3], "r") as f:
 
         # Read each number line by line
@@ -16,64 +80,35 @@ def main():
         
         # Add numbers to list until we run out (100 numbers)
         while line:
-            number.append(int(line))
+            A.append(int(line))
             line = f.readline()
     
-    print(number)
+    # Save the alg tag
+    alg = sys.argv[2]
     
+    # If prepartion alg, then pre-partition (turn A into A') and adjust alg tag
+    if len(alg) > 2:
+        alg = alg[-1]
+        A = prepart(A)
     
     match sys.argv[2]:
         case "0":
             # Karmarkar-Karp
-            karmarkarKarp(number)
+            res = karmarkarKarp(A)
         case "1":
             # Repeated Random
-            repeatedRandom(number)
+            res = repeatedRandom(A)
         case "2":
             # Hill Climbing
-            hillClimbing(number)
+            res = hillClimbing(A)
         case "3":
             # Simulated Annealing
-            simulatedAnnealing(number)
-        case "11":
-            # Prepartitioned Repeated Random
-            prepartitionedRepeatedRandom(number)
-        case "12":
-            # Prepartitioned Hill Climbing
-            prepartitionedHillClimbing(number)
-        case "13":
-            # Prepartitioned Simulated Annealing
-            prepartitionedSimulatedAnnealing(number)
-
+            res = simulatedAnnealing(A)
+            
+    # Print out the result for bbg auto grader
+    print(res)
     return None
 
 
-
-def karmarkarKarp(number):
-    return None
-
-
-def repeatedRandom(number):
-    return None
-
-
-def hillClimbing(number):
-    return None
-
-
-def simulatedAnnealing(number):
-    return None
-
-
-def prepartitionedRepeatedRandom(number):
-    return None
-
-
-def prepartitionedHillClimbing(number):
-    return None
-
-
-def prepartitionedSimulatedAnnealing(number):
-    return None
-    
-main()
+if __name__ == "__main__":
+    main()
